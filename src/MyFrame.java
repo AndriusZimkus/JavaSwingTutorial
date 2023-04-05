000b9ace2fb9e7f6fe89be0392e57160b22e0c95
AndriusZimkus <and.zimkus@gmail.com>
2023-04-05 22:54:44 +0300

    First commit of Swing Tutorial

---
+++ src/MyFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyFrame() {
+        this.setTitle("Temporary title");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(420, 420);
+//        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        ImageIcon image = new ImageIcon("skull.png");
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color(244, 239, 226));
+
+    }
+
+}
