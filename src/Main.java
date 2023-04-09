39fe0816e2e3ac963f3a508e6b58344c51d7bccb
AndriusZimkus <and.zimkus@gmail.com>
2023-04-09 23:39:19 +0300

    New Window

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import NewWindow.LaunchPage;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,7 +9,11 @@ public class Main {
 //        MyFrameBorderLayout myFrameBorderLayout = new MyFrameBorderLayout();
 //        MyFrameFlowLayout myFrameFlowLayout = new MyFrameFlowLayout();
 //        MyFrameGridLayout myFrameGridLayoutLayout = new MyFrameGridLayout();
-        MyFrameJLayeredPane myFrameJLayeredPane = new MyFrameJLayeredPane();
+//        MyFrameJLayeredPane myFrameJLayeredPane = new MyFrameJLayeredPane();
 
+// New window tutorial 2023-04-09
+        if (true) {
+            LaunchPage launchPage = new LaunchPage();
+        }
     }
 }
